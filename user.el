d3dcdc7eda4332fa6c193501b77a429ed3909e7f
Alex Lynham <acelynham@hotmail.com>
2014-11-26 10:12:33 +0000

    Remove audible bell.

--- user.el
+++ user.el
@@ -13,6 +13,7 @@
 (setq initial-frame-alist '((top . 0) (left . 0) (width . 100) (height . 80)))
 
 (setq visible-bell nil)
+(setq ring-bell-function 'ignore)
 
 ;; Place downloaded elisp files in this directory. You'll then be able
 ;; to load them.
